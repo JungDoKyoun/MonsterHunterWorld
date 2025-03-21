6685d57e0ec804d78fe8002ebd9abfdbb29580f7
jungyounghan <superbrush@naver.com>
2025-03-21 15:50:57 +0900

    [feat] PlayerCostume 번호 인덱스 추가

--- Assets/Scripts/PlayerCostume.cs
+++ Assets/Scripts/PlayerCostume.cs
@@ -1,17 +1,64 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [DisallowMultipleComponent]
 public class PlayerCostume : MonoBehaviour
 {
+    private enum Index: byte
+    {
+        HeadStart = 0,
+        HeadEnd = 1,
+        BreastStart = 2,
+        BreastEnd = 4,
+        HandStart = 5,
+        HandEnd = 7,
+        Waist = 8,
+        LegStart = 9,
+        LegEnd = 10
+    }
+
     [SerializeField]
-    private MeshRenderer _helmRenderers = null;
+    private SkinnedMeshRenderer[] _skinnedMeshRenderers = null;
 
 #if UNITY_EDITOR
     private void OnValidate()
     {
-        
+        Sort(ref _skinnedMeshRenderers);
     }
+
+    private void Sort<T>(ref T[] array) where T: Component
+    {
+        List<T> list = new List<T>();
+        int empty = 0;
+        int length = array != null ? array.Length : 0;
+        for (int i = 0; i < length; i++)
+        {
+            T value = array[i];
+            if (value != null)
+            {
+                if (list.Contains(value) == false)
+                {
+                    list.Add(value);
+                }
+                else
+                {
+                    empty++;
+                }
+            }
+            else
+            {
+                empty++;
+            }
+        }
+        for (int i = 0; i < empty; i++)
+        {
+            list.Add(null);
+        }
+        array = list.ToArray();
+    }
+
 #endif
+
 
     public void Set()
     {
