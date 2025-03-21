1785333bf0283fc163f9c430c501c6c12aba4abd
jungyounghan <superbrush@naver.com>
2025-03-21 10:12:15 +0900

    [feat] PlayerController 클래스 최적화
    PlayerStat 구조체 제거

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -40,11 +40,88 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
             return _animator;
         }
     }
+    private Coroutine _coroutine = null;
 
     private Vector3 _forward = Vector3.zero;
 
-    private Coroutine _coroutine = null;
+    [Header("최대 체력"), SerializeField]
+    private uint _fullLife;
 
+    public uint fullLife
+    {
+        get
+        {
+            return _fullLife;
+        }
+        set
+        {
+            _fullLife = value;
+            if (_fullLife < _currentLife)
+            {
+                _currentLife = _fullLife;
+            }
+        }
+    }
+
+    [Header("현재 체력"), SerializeField]
+    private uint _currentLife;
+
+    public uint currentLife
+    {
+        get
+        {
+            return _currentLife;
+        }
+        set
+        {
+            _currentLife = value;
+            if (_fullLife < _currentLife)
+            {
+                _currentLife = _fullLife;
+            }
+        }
+    }
+
+    [Header("최대 스태미너"), SerializeField, Range(MinStamina, MaxStamina)]
+    private float _fullStamina;
+
+    public float fullStamina
+    {
+        get
+        {
+            return _fullStamina;
+        }
+        set
+        {
+            _fullStamina = Mathf.Clamp(value, MinStamina, MaxStamina);
+            if (_fullStamina < _currentStamina)
+            {
+                _currentStamina = _fullStamina;
+            }
+        }
+    }
+
+    [Header("현재 스태미너"), SerializeField, Range(MinStamina, MaxStamina)]
+    private float _currentStamina;
+
+    public float currentStamina
+    {
+        get
+        {
+            return _currentStamina;
+        }
+        set
+        {
+            _currentStamina = Mathf.Clamp(value, MinStamina, MaxStamina);
+            if (_fullStamina < _currentStamina)
+            {
+                _currentStamina = _fullStamina;
+            }
+        }
+    }
+
+    private const float MinStamina = 0;
+    private const float MaxStamina = int.MaxValue;
     private static readonly float MinInput = -1;
     private static readonly float MaxInput = 1;
     private static readonly float RotationDamping = 10;
@@ -54,13 +131,23 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
     private static readonly string AttackTag = "Attack";
     private static readonly string RunClip = "Run";
 
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        
+    }
+#endif
+
     private void Update()
     {
-        if (photonView.IsMine == true)
+        if (photonView.IsMine == true && (_currentLife > 0 || _fullLife == _currentLife))
         {
             bool attack = Input.GetMouseButton(0);
             if (attack != getAnimator.GetBool(AttackTag))
             {
+#if UNITY_EDITOR
+                Debug.Log("공격:" + attack);
+#endif
                 Attack(attack);
                 if (PhotonNetwork.InRoom == true)
                 {
@@ -76,6 +163,9 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
                 bool jump = Input.GetButton(JumpTag);
                 if (jump == false && getAnimator.GetBool(JumpTag) == true)
                 {
+#if UNITY_EDITOR
+                    Debug.Log("점프 해제");
+#endif
                     Jump(false);
                     if (PhotonNetwork.InRoom == true)
                     {
@@ -101,22 +191,27 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
                                     _coroutine = StartCoroutine(DoMoveStart());
                                     IEnumerator DoMoveStart()
                                     {
+#if UNITY_EDITOR
+                                        Debug.Log("이동 시작");
+#endif
                                         Move();
                                         Vector3 forward = _forward;
                                         while (IsRunning() == false)
                                         {
-                                            Vector2 direction = new Vector2(Vector3.Dot(new Vector3(forward.z, forward.y, -forward.x), _forward), Vector3.Dot(forward, _forward));
-                                            if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
-                                            {
-                                                Move(direction);
-                                                if (PhotonNetwork.InRoom == true)
-                                                {
-                                                    photonView.RPC("Move", RpcTarget.Others, direction);
-                                                }
-                                                _coroutine = null;
-                                                yield break;
-                                            }
                                             yield return null;
+                                            Vector2 a = new Vector2(Vector3.Dot(new Vector3(forward.z, forward.y, -forward.x), _forward), Vector3.Dot(forward, _forward));
+                                            Vector2 b = new Vector2(Vector3.Dot(getTransform.right, _forward), Vector3.Dot(getTransform.forward, _forward));
+                                            if (Mathf.Abs(a.x) > Mathf.Abs(a.y) && (Mathf.Sign(a.x) != Mathf.Sign(b.x) || Mathf.Sign(a.y) != Mathf.Sign(b.y)))
+                                            {
+                                                Debug.Log(getAnimator.GetFloat(HorizontalTag) + " " + getAnimator.GetFloat(VerticalTag));
+                                                //Debug.Log(a + " " + b);
+                                                //Move(a);
+                                                //if (PhotonNetwork.InRoom == true)
+                                                //{
+                                                //    photonView.RPC("Move", RpcTarget.Others, a);
+                                                //}
+                                                forward = _forward;
+                                            }
                                         }
                                         while (IsRunning() == true)
                                         {
@@ -134,6 +229,9 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
                                     StopCoroutine(_coroutine);
                                     _coroutine = null;
                                 }
+#if UNITY_EDITOR
+                                Debug.Log("이동 멈춤");
+#endif
                                 _forward = Vector3.zero;
                                 Move();
                             }
@@ -153,7 +251,9 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
                             {
                                 _forward = getTransform.forward;
                             }
-                            //Debug.Log(new Vector2(Vector3.Dot(getTransform.right, _forward), Vector3.Dot(getTransform.forward, _forward)));
+#if UNITY_EDITOR
+                            Debug.Log("구르기");
+#endif
                             Move();
                             Jump(true);
                             if (PhotonNetwork.InRoom == true)
@@ -169,6 +269,7 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
 
     private void OnTriggerEnter(Collider other)
     {
+
     }
 
     private void RotateLeft()
@@ -227,6 +328,16 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
             }
         }
         return false;
+    }
+
+    public void TakeDamage(Vector3 position, uint damage)
+    {
+
+    }
+
+    public void Heal(uint value)
+    {
+
     }
 
     public override void OnDisable()
