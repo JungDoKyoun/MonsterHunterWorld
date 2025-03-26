ae956c0bda01ae0d9e21a3d66ce4b3781caf6083
Sonseongtae <sonseongtae@gmail.com>
2025-03-26 10:42:16 +0900

    [Feat] 싱글룸 요구 기능들, 집회소로 이동까지 완료
    로그인 성공 후 싱글룸으로 이동, 퀘스트 생성, 취소, 시작 가능. 집회소로 이동가능한 위치에서 F키를 누르면 집회소로 이동

--- Assets/Scripts/SST/Network/MeetingHouseCtrl.cs
+++ Assets/Scripts/SST/Network/MeetingHouseCtrl.cs
@@ -1,11 +1,12 @@
 using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 // ▼ 집회소에 들어가는 상호작용을 위한 스크립트
-public class MeetingHouseCtrl : MonoBehaviour
+public class MeetingHouseCtrl : MonoBehaviourPunCallbacks
 {
     private Collider meetingHouseZone;
 
@@ -18,12 +19,30 @@ public class MeetingHouseCtrl : MonoBehaviour
 
     private void Update()
     {
-        if(isInMeetingZone && Input.GetKeyDown(KeyCode.F))
+        if (isInMeetingZone && Input.GetKeyDown(KeyCode.F))
         {
-            PhotonNetwork.LeaveRoom();
+            StartCoroutine(WaitForCreateMeetingRoom());            
+        }
+    }
 
-            SceneManager.LoadScene("MeetingHouse");
-        }                   
+    IEnumerator WaitForCreateMeetingRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        while (!PhotonNetwork.IsConnectedAndReady)
+        {
+            yield return null;
+        }
+
+        Debug.Log("방 생성할 준비가 되었습니다. 집회소로 이동합니다.");
+
+        RoomOptions roomOptions = new RoomOptions { MaxPlayers = 16 };
+        PhotonNetwork.CreateRoom("MeetingHouse",roomOptions);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("방 입장");
+        SceneManager.LoadScene("MeetingHouse");
     }
 
     private void OnTriggerEnter(Collider other)
