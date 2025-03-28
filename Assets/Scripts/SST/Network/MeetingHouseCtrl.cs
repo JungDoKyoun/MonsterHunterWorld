ab59f8e1c355f4aebd7e15acd6c2283edc098bd7
Sonseongtae <sonseongtae@gmail.com>
2025-03-28 09:30:45 +0900

    [Fix] singleRoom 씬 수정, 상호작용 UI 수정중

--- Assets/Scripts/SST/Network/MeetingHouseCtrl.cs
+++ Assets/Scripts/SST/Network/MeetingHouseCtrl.cs
@@ -20,8 +20,7 @@ public class MeetingHouseCtrl : MonoBehaviourPunCallbacks
     private void Start()
     {
         meetingHouseZone = GetComponent<Collider>();
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
-        targetPanel = player.transform.Find("Move Panel");
+        StartCoroutine(WaitForFindPlayer());
     }
 
     private void Update()
@@ -30,6 +29,26 @@ public class MeetingHouseCtrl : MonoBehaviourPunCallbacks
         {           
             StartCoroutine(WaitForCreateMeetingRoom());            
         }
+    }
+
+    IEnumerator WaitForFindPlayer()
+    {
+        // 플레이어를 못찾았다면 반복
+        while(player == null)
+        {
+            // 못찾았다면 태그가 플레이어인 게임오브젝트를 찾아서 임시 변수에 담는다.
+            GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+
+            // 임시 게임오브젝트 변수에 담겼다면
+            if(playerObj != null)
+            {
+                // player에 플레이어 컴포넌트를 담는다
+                player = playerObj.GetComponent<PlayerController>();
+            }
+            yield return null;
+        }
+        // 플레이어 자식에 있는 Move Panel을 찾아서 담아준다
+        targetPanel = player.transform.Find("Move Panel");
     }
 
     IEnumerator WaitForCreateMeetingRoom()
