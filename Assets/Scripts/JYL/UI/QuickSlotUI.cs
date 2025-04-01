7a2f00fdebf80f229b98f8109889393df33b14d7
jylack <jyl011202@gmail.com>
2025-04-01 11:59:08 +0900

    [fix] 싱글룸에서 인벤 생성 시작하게해둠

--- Assets/Scripts/JYL/UI/QuickSlotUI.cs
+++ Assets/Scripts/JYL/UI/QuickSlotUI.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuickSlotUI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Text useItemCountText;
+    [SerializeField] Image itemImg;
+    [SerializeField] Text itemNameText;
+
+    BaseItem item;
+
+    int minIndex = 0;
+    int maxIndex = 0;
+    int currentIndex = 0;
+    
+    bool isSelecting = false;
+    
     void Start()
     {
-        
+        item = InvenToryCtrl.Instance.inventory[0];
+
+        useItemCountText.text = item.count.ToString();
+        itemNameText.text = item.name;
+        itemImg.sprite = item.image;
+        itemImg.color = item.color;
+
+        maxIndex = InvenToryCtrl.Instance.inventory.Count;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void NextItem(int index)
     {
-        
+        if (index < minIndex)
+        {
+            return;
+        }
+
+        currentIndex = index;
+
+        item = InvenToryCtrl.Instance.inventory[currentIndex];
     }
+
+    void PrevItem(int index)
+    {
+        if (index >= maxIndex)
+        {
+            return;
+        }
+
+        currentIndex = index;
+
+        item = InvenToryCtrl.Instance.inventory[currentIndex];
+    }
+
 }
