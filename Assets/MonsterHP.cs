a752ed01e03bbb61b9837b05b2864fa01304dee8
jylack <jyl011202@gmail.com>
2025-04-03 15:46:36 +0900

    [feat] 몬스터 체력 테스트용 UI 생성

---
+++ Assets/MonsterHP.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterHP : MonoBehaviour
+{    
+    
+    void Start()
+    {
+        MonsterController target = FindObjectOfType<MonsterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
