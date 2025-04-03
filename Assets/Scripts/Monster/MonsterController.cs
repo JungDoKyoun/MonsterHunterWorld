f7911a2d5fc2cbb9df00d7f217dfc543420e04df
JungDoKyoun <kyous112@naver.com>
2025-04-03 14:15:30 +0900

    [feat] 동기화 해결

--- Assets/Scripts/Monster/MonsterController.cs
+++ Assets/Scripts/Monster/MonsterController.cs
@@ -56,6 +56,8 @@ public class MonsterController : MonoBehaviourPunCallbacks
     private float _minAttackRange; //백무브 하는 범위
     private float _sturnTime; //스턴 지속 시간
     private float _trapTime; //트랩 지속 시간
+    private double sleepStartTime;
+    private double wakeUpTime;
     private bool _isRo; //회전중인지?
     private bool _isDie; //죽었는지?
     private bool _isHit; //맞았는지?
@@ -1004,12 +1006,15 @@ public class MonsterController : MonoBehaviourPunCallbacks
     {
         if (!PhotonNetwork.IsMasterClient) return;
 
-        photonView.RPC("Sleep", RpcTarget.All);
+        double currentNetworkTime = PhotonNetwork.Time;
+        photonView.RPC("Sleep", RpcTarget.All, currentNetworkTime);
     }
 
     [PunRPC]
-    public void Sleep()
+    public void Sleep(double startTime)
     {
+        sleepStartTime = startTime;
+        wakeUpTime = startTime + SleepTime;
         StartCoroutine(WaitForEndSleepAnime());
     }
 
@@ -1236,17 +1241,21 @@ public class MonsterController : MonoBehaviourPunCallbacks
 
     public IEnumerator WaitForEndSleepAnime()
     {
+        yield return new WaitUntil(() =>
+        _anime != null &&
+        _anime.isInitialized &&
+        _anime.runtimeAnimatorController != null &&
+        _anime.gameObject.activeInHierarchy
+    );
+
         yield return new WaitForSeconds(0.3f);
         yield return new WaitUntil(() => _anime.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f);
         SetAnime("IsSleep2", true);
-        float tempTime = 0;
-        while(tempTime <= _sleepTime)
+
+        while (PhotonNetwork.Time < wakeUpTime)
         {
-            tempTime += Time.deltaTime;
             yield return null;
         }
-        Debug.Log("자는거 끝");
-        SetAnime("IsSleep2", false);
         _isSleep = false;
     }
 
